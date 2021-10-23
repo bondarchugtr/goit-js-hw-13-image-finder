327de51c2a1d39b5d61f1ab516758a92dc59e638
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-10-23 03:08:42 +0300

    +

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import apiService from './apiService';
 import cardImage from '../tamplate/image-card.hbs';
-// import cardList from '../tamplate/list-gallery.hbs';
 import cardForm from '../tamplate/form.hbs';
 import refs from './refs.js'
 import { debounce } from 'debounce';
@@ -8,31 +7,34 @@ import { debounce } from 'debounce';
 const { main, form, btnOpenImage, galleryItem } = refs;
 renderformSearch()
 
+const searchForm = document.querySelector('form');
+let page = 1;
+let searchQuery = ''
+
+function remuveHidden() {
+    btnOpenImage.classList.remove('hidden')
+}
+
 //form search
 function renderformSearch(form) {
     const marcup = cardForm(form)
     main.insertAdjacentHTML("afterbegin", marcup)
 }
 
-
-//list foto
-// function renderListCard(card) {
-//     const marcup = cardList(card);
-//     main.insertAdjacentHTML("beforeend", marcup);
-// }
-let page = 1;
-
-let searchQuery = ''
 //foto card
 function renderFotoCard(card) {
     const marcup = cardImage(card.hits);
-    galleryItem.insertAdjacentHTML('beforeend', marcup)
-    console.log(marcup);
+    if (page === 1) {
+        galleryItem.innerHTML = marcup;
+    } else {
+        btnOpenImage.classList.remove('.hidden')
+        galleryItem.insertAdjacentHTML('beforeend', marcup)
+    }
+
 }
 
 //input value
 function onInputNameFoto() {
-    const searchForm = document.querySelector('form');
     searchForm.addEventListener('submit', e => {
         e.preventDefault()
         page = 1;
@@ -42,30 +44,44 @@ function onInputNameFoto() {
 }
 onInputNameFoto()
 
-
 function renderCard(searchQuery, page) {
     apiService(searchQuery, page)
         .then(data => {
             renderFotoCard(data)
             onBtnOpenImg()
+            remuveHidden()
+            setTimeout(
+                () =>
+                    window.scrollTo({
+                        top: document.documentElement.offsetHeight,
+                        behavior: 'smooth',
+                    }), 500)
         })
         .catch(() => {
             alert('error')
         });
 }
 
+// const intersectionObserver = new IntersectionObserver(function (entries) {
+//     console.log('object');
+//     if (entries[0].intersectionRatio <= 0) return;
+//     loadItems(10);
+//     console.log('Loaded new items');
+// });
+// // начать наблюдение
+// intersectionObserver.observe(document.querySelector('.scrollerFooter'));
+
+
 
 function onBtnOpenImg() {
     const btnOpenImage = document.querySelector('.button__open')
+    btnOpenImage.classList.remove('.hidden')
     btnOpenImage.addEventListener('click', el => {
-
         page += 1
         console.log(page);
         renderCard(searchQuery, page)
     })
 }
-
-
 
 // export class APIpexel {
 //     constructor({ root = 'main' }) {
